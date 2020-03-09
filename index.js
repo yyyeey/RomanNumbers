055bed2691aba49c69007793c6bf06815a36c933
Daniel Grad <graddaniel0@gmail.com>
2020-03-09 19:20:54 +0100

    2nd 10min;

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const RomanNumber = function(value) {
+    const arabicToRomanMap = [
+
+    ];
+
     if(!new.target)
         return new RomanNumber(value)
 
+    if (!value) {
+        //throw error
+    } else if (typeof value === 'number') {
+        this.number = value;
+    } else if (typeof value === 'string') {
+        this.string = value;
+    } else {
+        //throw some error
+    }
     //check input
     //parse input
     //check for small before big
     //make sure theres a combination of 1 small and 1 big  pair at a time
+    //no more than 3x same char
+    //only multiples of 10 and 1 can be subtracted
+
+    //TODO: remove V
     this.string = 'RomanNumber.string';
     this.number = 123;
 }
@@ -32,4 +49,5 @@ Error(‘value required’);’ )
 ● Is outside of 1 to 3999, it should throw an ‘invalid range’ exception error.
 ● Is invalid, it should throw an ‘invalid value’ exception error.
 
+exceptions don't stop the code exec
 */
