9b2f1d50d0db7502c7df445b77137d72e32f6350
Daniel Grad <graddaniel0@gmail.com>
2020-03-09 19:50:37 +0100

    5th 10min;
    Added ArabicToRoman conversion
    Checking if it works properly

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const RomanNumber = function(value) {
     const VALUE_RANGE = {MIN: 1, MAX: 3999}
     const arabicToRomanMap = [
-        {arabic: 1, roman: 'I'},
-        {arabic: 4, roman: 'IV'},
-        {arabic: 5, roman: 'V'},
-        {arabic: 9, roman: 'IX'},
-        {arabic: 10, roman: 'X'},
-        {arabic: 40, roman: 'XL'},
-        {arabic: 50, roman: 'L'},
-        {arabic: 90, roman: 'XC'},
-        {arabic: 100, roman: 'C'},
-        {arabic: 400, roman: 'CD'},
-        {arabic: 500, roman: 'D'},
-        {arabic: 900, roman: 'CM'},
         {arabic: 1000, roman: 'M'},
+        {arabic: 900, roman: 'CM'},
+        {arabic: 500, roman: 'D'},
+        {arabic: 400, roman: 'CD'},
+        {arabic: 100, roman: 'C'},
+        {arabic: 90, roman: 'XC'},
+        {arabic: 50, roman: 'L'},
+        {arabic: 40, roman: 'XL'},
+        {arabic: 10, roman: 'X'},
+        {arabic: 9, roman: 'IX'},
+        {arabic: 5, roman: 'V'},
+        {arabic: 4, roman: 'IV'},
+        {arabic: 1, roman: 'I'},
     ];
 
     if(!new.target)
@@ -22,42 +22,56 @@ const RomanNumber = function(value) {
     if (!value) {
         throw new Error('value required'); //the value cannot be empty
     } else if (typeof value === 'number') {
+        console.log(value)
         if (value < VALUE_RANGE.MIN || value > VALUE_RANGE.MAX) {
             throw new Error('invalid range');
         }
 
-        this.number = value;
+        this.arabic = value;
 
-        // this.string = 
+        let remainder = value;
+        let romanString = '';
+        while (remainder > 0) {
+            const token = arabicToRomanMap.find(e => e.arabic <= remainder);
+            console.log(token, remainder)
+            if(!token) {
+                throw new Error('invalid value');
+            }
+
+            romanString += token.roman;
+            remainder -= token.arabic;
+        }
+        this.roman = romanString;
+
     } else if (typeof value === 'string') {
-        this.string = value;
+        this.roman = value;
 
         //check for small before big
         //make sure theres a combination of 1 small and 1 big  pair at a time
         //no more than 3x same char
         //only multiples of 10 and 1 can be subtracted
-        //this.number = 
+        //this.arabic = 
     } else {
         throw new Error('invalid value');
     }
 }
 
 RomanNumber.prototype.toString = function() {
-    return this.string;
+    return this.roman;
 }
 
 RomanNumber.prototype.toInt = function() {
-    return this.number;
+    return this.arabic;
 }
 
 let romanNumber1 = new RomanNumber('XX');
 let romanNumber2 = new RomanNumber(40);
+let r1 = new RomanNumber(1995);
+let r2 = new RomanNumber(432);
+let r3 = new RomanNumber(234);
+let r4 = new RomanNumber(989);
 
-console.log(romanNumber1.toInt(), romanNumber1.toString())
-let a = "12345"
-for(let i = 0; i < a.length; ++i) {
-    console.log(a[i])
-}
+console.log(romanNumber1.toInt(), romanNumber1.toString(), r1.toString(), r2.toString(), r3.toString(), r4.toString())
 
 /*Test for:
 null, ‘’, 0, 1, 3, 4, 5, ‘I’, ‘III’, ‘IIII’, ‘IV’, ‘V’, 1968, ‘1473’, 2999, 3000, 10000, ‘CDXXIX’, ‘CD1X’,
